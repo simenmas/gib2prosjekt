4957874f8312da2eab7ed61aede29124e25f26ea
jshjelse <jshjelse@stud.ntnu.no>
2023-05-01 10:22:51 +0200

    Fix

--- gibProsjekt/GibProsjekt/mapapp/models.py
+++ gibProsjekt/GibProsjekt/mapapp/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Punkt(models.Model):
-    tittel = models.CharField(max_length=30)
-    lat = models.DecimalField(decimal_places=3, max_digits=5)
-    lon = models.DecimalField(decimal_places=3, max_digits=5)
-    typ = models.CharField(max_length=20)
-    synlighet = models.CharField(max_length=30)
-    beskrivelse = models.CharField(max_length=200)
